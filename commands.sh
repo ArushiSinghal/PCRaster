6e90b38acde78cce80f09ddb3a34c5265f18a482
Arushi Singhal <arushisinghal@gmail.com>
2017-10-20 15:43:14 +0530

    changes

--- commands.sh
+++ commands.sh
@@ -1,3 +1,4 @@
 export PATH=/opt/pcraster-4.1.0_x86-64/bin:$PATH
 export PYTHONPATH=/opt/pcraster-lsbcc-4_86-64/python:$PYTHONPATH
-cd ~/pcraster_demo_data-20131204/workspace/demo/data/
+cd pcraster_demo_data-20131204/workspace/demo/data/
+firefox http://pcraster.geo.uu.nl/documentation/Demo/DynamicModellingDemo.html
